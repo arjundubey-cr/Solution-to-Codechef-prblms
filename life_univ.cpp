7491460762ff1717868e5045711421feaccc9997
arjundubey-cr <40758425+arjundubey-cr@users.noreply.github.com>
2018-08-04 17:37:23 +0530

    Create life_univ.cpp

---
+++ life_univ.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while (n != 42)
+    {
+        cout << n << endl;
+        cin >> n;
+        // The cin command will prompt the system for further input.
+    }
+}
